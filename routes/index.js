1ddda6882c995e919eebbf04af231c70cfb1f390
Bea <chamoischamois@protonmail.com>
2022-07-03 14:47:32 +0800

    Added form so users can submit a message to the board

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: 'Hello!',
+    user: 'Test User',
+    added: new Date()
+  },
+  {
+    text: 'Hi everyone',
+    user: 'Sandy',
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Message Board', messages: messages });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'Message Board' });
+});
+
+router.post('/new', function(req, res, next) {
+  const userMessage = req.body.userMessage;
+  const userName = req.body.userName;
+  messages.push({text: userMessage, user: userName, added: new Date()});
+  res.redirect('/');
 });
 
 module.exports = router;
